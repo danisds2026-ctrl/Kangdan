638e52cffdd73b8f1788e93d98cac1978ea604c0
danisds2026-ctrl <danisds2026@gmail.com>
2026-08-12 07:39:30 +0700

    Create killer.bat to terminate specific processes

---
+++ killer.bat
@@ -0,0 +1,21 @@
+taskkill /f /im supervisord.exe
+
+taskkill /f /im AtUpDownProNode.exe
+
+taskkill /f /im secure.exe
+
+taskkill /f /im testDanger.exe
+
+taskkill /f /im AtSocket.exe
+
+taskkill /f /im aria2c.exe
+
+taskkill /f /im AtDeskTop.exe
+
+taskkill /f /im AtGameLauncherNode.exe
+
+taskkill /f /im websocket.exe
+
+taskkill /f /im categraf.exe
+
+pause
